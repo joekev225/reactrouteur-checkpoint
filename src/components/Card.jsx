5be47615f051f2806b227553bba6b811ad526e97
joekev225 <ouragajoelkevin@gmail.com>
2023-05-25 12:56:44 +0000

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ function Card(props) {
   const handleClick = () => {}
   return (
     <div className="card" >
-        <iframe width="100%" height="300" src={props.films.link} title={props.film.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullscreen></iframe>
-        <h3>{props.films.title}</h3> 
+        <iframe width="560" height="315" src={props.film.postUrl} title={props.film.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <h3>{props.film.title}</h3> 
         <div className="card" onClick={handleClick}>
         {/* Contenu de la carte */}
         </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ function Card(props) {
 
     return (
         <div className="card" >
-            <iframe width="100%" height="300" src={props.films.link} title={props.film.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullscreen></iframe>
+            <iframe width="100%" height="300" src={props.film.link} title={props.film.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullscreen></iframe>
             <h3>{props.film.title}</h3>
             <p className='describe'>{props.film.description}</p>
             <p>rate: {props.film.rating}/5</p>
